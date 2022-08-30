2154d7ee4c6be55afa278ac1b0419f1ee3fbafa6
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2022-08-30 10:39:04 +0700

    up

--- services/users/go.mod
+++ services/users/go.mod
@@ -2,4 +2,7 @@ module github.com/marfanr/simpleonlineshop/services/users
 
 go 1.18
 
-require github.com/go-chi/chi/v5 v5.0.7
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	google.golang.org/protobuf v1.28.1
+)
--- services/users/main.go
+++ services/users/main.go
@@ -10,11 +10,14 @@ import (
 
 func main() {
 	r := chi.NewRouter()
+	// set up for grpc
+
 	var port = ":3535"
 	r.Use(middleware.Logger)
 	r.Get("/api/v1/get", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
+	// run servers
 	fmt.Println("Service running on", port)
 	http.ListenAndServe(port, r)
 }
